0be72e428b8cd14f26d937cc0fb034c6c60fb205
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-13 13:31:11 -0300

    subindo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        private const string V = "false";
+        private const string V1 = "False";
+
+        static void Main(string[] args)
+        {
+           
+            try{
+                bool permitir;
+                bool cases=bool.Parse(Console.ReadLine());
+                permitir=cases;
+                
+            }catch{
+                Console.WriteLine("ERRO");
+            }
+
+        }
+    }
+}
